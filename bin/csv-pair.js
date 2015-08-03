816b3bf32719cbac3c1d85af15b7224aa1dec1c7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-08-03 15:17:40 -0400

    bin

--- bin/csv-pair.js
+++ bin/csv-pair.js
@@ -3,3 +3,16 @@
 require('lazy-ass');
 var check = require('check-more-types');
 
+if (process.argv.length !== 5) {
+  console.log('Usage: <filename1.csv> <filename2.csv> <output.csv>');
+  process.exit(-1);
+}
+
+var pair = require('../src/csv-pair');
+la(check.fn(pair), 'expected function', pair);
+
+var inputFilenames = [
+  process.argv[2],
+  process.argv[3]
+];
+pair(inputFilenames, process.argv[4]);
--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,9 @@
 require('lazy-ass');
 var check = require('check-more-types');
+var R = require('ramda');
+var fromFolder = R.unary(R.partial(require('path').join, __dirname));
+var exists = require('fs').existsSync;
+var unlink = require('fs').unlinkSync;
 
 describe('csv-pair', function () {
   var pair;
@@ -10,6 +14,19 @@ describe('csv-pair', function () {
 
   it('is a function', function () {
     la(check.fn(pair));
+  });
+
+  it('combines two CSV files', function () {
+    var filenames = ['1-5-days.csv', '1-10-days.csv'].map(fromFolder);
+    var saveFilename = fromFolder('combined.csv');
+
+    if (exists(saveFilename)) {
+      unlink(saveFilename);
+    }
+    la(!exists(saveFilename), 'do not have the output file', saveFilename);
+
+    pair(filenames, saveFilename);
+    la(exists(saveFilename), 'found created file', saveFilename);
   });
 
 });
